70fe554bfe046993cb318d2d6b3570d6c33356ec
Juan Pablo De Anda Rios <jpsilvernova@gmail.com>
2024-12-10 21:55:48 -0600

    Segundo Commit

---
+++ cart-app/src/cartApp.jsx
@@ -0,0 +1,24 @@
+import { CartView } from "./components/CartView"
+import { CatalogView } from "./components/CatalogView"
+import { useItemsCart } from "./hooks/useItemsCart"
+
+export const CartApp = () => {
+
+    const { cartItems, handlerAddProductCart, handlerDeleteProductCart} = useItemsCart();
+
+    return (
+        <>
+            <div className="container my-4">
+                <h3>Cart-App</h3>
+                <CatalogView handler={handlerAddProductCart} />
+
+                { cartItems?.length <= 0 || 
+                    (
+                        <div className="my-4 w-50">
+                            <CartView items={cartItems} handlerDelete={handlerDeleteProductCart} />
+                        </div>
+                    )}
+            </div>
+        </>
+    )
+}
---
+++ cart-app/src/data/product.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecánico RGB',
+        description: 'Teclado Mecánico con colores RBG y switches cherry red',
+        price: 1000
+    },
+    {
+        id: 2,
+        name: 'Samsumg Smart TV 55 pulgadas',
+        description: 'Smart TV 4K 120hz ',
+        price: 2000
+    },
+    {
+        id: 3,
+        name: 'Audifonos Bluetooth Sony',
+        description: 'Audifonos Bluetooth high fidelity y Noise canceling',
+        price: 600
+    },
+    {
+        id: 4,
+        name: 'Memoria Ram Corsair 8GB DDR5',
+        description: 'Memoria RAM 3200Mhz optimizada para gaming',
+        price: 800
+    },
+    {
+        id: 5,
+        name: 'Tarjeta Gráfica ASUS NVIDIA RTX',
+        description: 'Tarjeta grafica para juegos en 4K',
+        price: 4000
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core i5',
+        description: 'Optimizado para multitasking',
+        price: 3000
+    }
+]
